6bee93cc0a8d0e5f41b5578cc34375588d9f7b29
zlfn <elusive1102@naver.com>
2021-10-11 02:29:45 +0900

    Add documentation comment for GetBackendIP()

--- src/DataManager/mod.rs
+++ src/DataManager/mod.rs
@@ -1,4 +1,11 @@
 pub mod FileIO;
 const BACKIP : &str = "127.0.0.1";
 
+///백엔드의 IP 주소를 Stirng으로 반환합니다.
+///
+/// # Example
+/// ```
+/// let ip = DataManager::GetBackendIP();
+/// ```
+///
 pub fn GetBackendIP() -> String {String::from(BACKIP)}
